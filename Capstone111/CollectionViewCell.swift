8d89398ca51be1d62f2c83e626f082f543bf9f27
annalali <aa601680@gmail.com>
2022-09-12 08:10:07 +0300

    Updaut by Annal

---
+++ Capstone111/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  Capstone111
+//
+//  Created by Anaal Albeeshi on 15/02/1444 AH.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    // var imageView =
+    
+    @IBOutlet weak var ImageView: UIImageView!
+}
